bb3ac20c4ca42bd4aeae7662b54d2348998ca698
Srijan Samridh <srijansinha3@gmail.com>
2022-01-28 04:25:38 +0530

    round_icon_button.dart imported

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -16,8 +16,12 @@ class ResultsPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const Expanded(
-            child: Text("Your Result", style: kTitleTextStyle),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(15.0),
+              alignment: Alignment.bottomLeft,
+              child: const Text("Your Result", style: kTitleTextStyle),
+            ),
           ),
           Expanded(
             flex: 6,
