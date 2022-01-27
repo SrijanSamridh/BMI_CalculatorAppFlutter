e215157f6cdc37a8b9d80fee3e352a856cd2c163
Srijan Samridh <srijansinha3@gmail.com>
2022-01-28 02:40:58 +0530

    round_icon_button.dart update

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -5,6 +5,7 @@ import 'reuse_container.dart';
 import 'bottom_button.dart';
 import 'constants.dart';
 import 'results_page.dart';
+import 'round_icon_button.dart';
 
 enum Gender { male, female }
 
@@ -230,27 +231,5 @@ class _InputPageState extends State<InputPage> {
             ),
           ],
         ));
-  }
-}
-
-class RoundIconButton extends StatelessWidget {
-  const RoundIconButton({Key? key, required this.icon, required this.onPressed})
-      : super(key: key);
-  // const RoundIconButton({ this.onPressed})
-  final IconData icon;
-  final VoidCallback onPressed;
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-      onPressed: onPressed,
-      elevation: 6.0,
-      constraints: const BoxConstraints.tightFor(
-        width: 56.0,
-        height: 56.0,
-      ),
-      shape: const CircleBorder(),
-      fillColor: kActiveContainerColor,
-    );
   }
 }
